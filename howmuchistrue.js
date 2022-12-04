23d38186ad5283dd7f45d72cf858f2420d938efe
rondolphstm <rondolphtst.m@gmail.com>
2022-12-04 00:52:07 -0500

    edaabit array practice

--- howmuchistrue.js
+++ howmuchistrue.js
@@ -1,1 +1,17 @@
 // Create a function which returns the number of true values there are in an array.
+
+// EX:
+// countTrue([true, false, false, true, false]) ➞ 2
+
+// countTrue([false, false, false, false]) ➞ 0
+
+// countTrue([]) ➞ 0
+
+let arr = countTrue([true, false, false, true, false])
+
+function countTrue(arr) {
+	// arr.sort(true)
+    for(let i=0; i < arr.length; i++){
+     return arr.sort(true)
+    }
+}console.log(arr)
